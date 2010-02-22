31a374baa24089a6fc12070c10909e6b0f0c4322
Continuous Integration <oliver.stauss@web.de>
2010-02-22 16:29:57 +0100

    * fist sample "hello-world" added.

---
+++ examples/hello-world/src/main/java/de/exxcellent/jax/App.java
@@ -0,0 +1,13 @@
+package de.exxcellent.jax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
